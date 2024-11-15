ccb02bc6b6da9f7e8bd939dc79a5c95abd641f90
Uchenna Obi <greenfieldsolutions32@gmail.com>
2024-11-15 10:54:20 +1000

    updated token utils

--- src/api/utils/token.util.ts
+++ src/api/utils/token.util.ts
@@ -1,22 +1,4 @@
-import jwt from 'jsonwebtoken'
-import { IUser } from '../interfaces'
-import { JWT_SECRET_KEY } from '@configs';
-
-export const generateToken = async (payload: Partial<IUser>, expiresIn: string) => {
-    return jwt.sign(payload, JWT_SECRET_KEY, { expiresIn })
-}
-
-export const verifyToken = async (token: string) => {
-    const decoded = <jwt.JwtPayload>jwt.decode(token);
-    if (!decoded) return 'invalid'
-    if (new Date(<number>decoded?.exp * 1000) <= new Date()) return 'expired'
-
-    try {
-        return jwt.verify(token, JWT_SECRET_KEY)
-    } catch (e) {
-        return 'invalid'
-    }
-}import jwt, { JwtPayload } from 'jsonwebtoken'
+import jwt, { JwtPayload } from 'jsonwebtoken'
 import { IUser } from '../interfaces'
 import { JWT_SECRET_KEY } from '@configs';
 import { NotFoundException, UnAuthorizedException, userService } from '@services';
